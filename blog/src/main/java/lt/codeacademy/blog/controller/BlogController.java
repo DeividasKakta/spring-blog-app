d1c53d39475ad60bb4e60e17617a9d7fc740c0fd
Deividas Kakta <deividaskakta@gmail.com>
2021-04-20 17:41:36 +0300

    Create initial blog template, controller fixes

--- blog/src/main/java/lt/codeacademy/blog/controller/BlogController.java
+++ blog/src/main/java/lt/codeacademy/blog/controller/BlogController.java
@@ -1,5 +1,6 @@
 package lt.codeacademy.blog.controller;
 
+import lombok.RequiredArgsConstructor;
 import lt.codeacademy.blog.service.BlogService;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -7,11 +8,14 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/blogs")
+@RequiredArgsConstructor
 public class BlogController {
 
-    private BlogService blogService;
+    private final BlogService blogService;
 
     @GetMapping
     public String getBlogs(@PageableDefault(size = 5, sort = {"date"}, direction = Sort.Direction.ASC) Pageable pageable,
@@ -20,4 +24,5 @@ public class BlogController {
 
         return "blogs";
     }
+
 }
