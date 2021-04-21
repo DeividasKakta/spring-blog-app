9a6bb974057dbd99827e7b8774811c0a7b3f4d3a
Deividas Kakta <deividaskakta@gmail.com>
2021-04-21 16:34:45 +0300

    Update blogs template - add navbar and content generation

--- blog/src/main/java/lt/codeacademy/blog/model/Blog.java
+++ blog/src/main/java/lt/codeacademy/blog/model/Blog.java
@@ -3,6 +3,7 @@ package lt.codeacademy.blog.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import java.util.Date;
@@ -15,6 +16,7 @@ public class Blog extends BaseEntity {
 
     private String title;
 
+    @Column(columnDefinition = "VARCHAR(1500)")
     private String content;
 
     private Date date;
