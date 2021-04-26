052cc69a50f9625e13598c7201ffb88a2c829e31
Deividas Kakta <deividaskakta@gmail.com>
2021-04-26 15:27:33 +0300

    Add simple validation to blog creation

--- blog/src/main/java/lt/codeacademy/blog/controller/BlogController.java
+++ blog/src/main/java/lt/codeacademy/blog/controller/BlogController.java
@@ -8,9 +8,12 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/blogs")
@@ -35,7 +38,10 @@ public class BlogController {
     }
 
     @PostMapping("/create")
-    public String createBlog(Blog blog) {
+    public String createBlog(@Valid Blog blog, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "createBlog";
+        }
         blogService.addBlog(blog);
 
         return "redirect:/blogs";
--- blog/src/main/java/lt/codeacademy/blog/model/Blog.java
+++ blog/src/main/java/lt/codeacademy/blog/model/Blog.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
 import java.util.Date;
 
 @Getter
@@ -14,9 +15,11 @@ import java.util.Date;
 @Table(name = "BLOGS")
 public class Blog extends BaseEntity {
 
+    @NotBlank
     private String title;
 
     @Column(columnDefinition = "VARCHAR(1500)")
+    @NotBlank
     private String content;
 
     private Date date = new Date();
