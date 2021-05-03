42cd949f858c19c8e7183a8a6cb695866c1791d2
Deividas Kakta <deividaskakta@gmail.com>
2021-05-03 16:55:00 +0300

    Add UserController and register/login templates

---
+++ blog/src/main/java/lt/codeacademy/blog/controller/UserController.java
@@ -0,0 +1,22 @@
+package lt.codeacademy.blog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class UserController {
+
+    @GetMapping("/register")
+    public String openRegister() {
+
+        return "register";
+    }
+
+    @GetMapping("/login")
+    public String openLogin() {
+
+        return "login";
+    }
+}
